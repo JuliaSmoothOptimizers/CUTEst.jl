f96b2adf970b2aea6e8c722eff91fd6777dccbea
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2015-02-11 19:58:56 -0200

    Add missing PATH information and refactor travis

--- before_install.sh
+++ before_install.sh
@@ -2,15 +2,16 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo apt-get update -qq -y
   sudo apt-get install -y gfortran
   git clone https://github.com/Homebrew/linuxbrew.git $TRAVIS_BUILD_DIR/linuxbrew
-  brewcmd=$TRAVIS_BUILD_DIR/linuxbrew/bin/brew
-  $brewcmd tap dpo/cutest
-  $brewcmd install cutest --HEAD
-  $brewcmd install mastsif --HEAD
+  PATH=$PATH:$TRAVIS_BUILD_DIR/linuxbrew/bin/
+  brew tap dpo/cutest
+  brew install cutest --HEAD
+  brew install mastsif --HEAD
+  echo "export PATH=$PATH" > $TRAVIS_BUILD_DIR/src_vars
   for f in archdefs mastsif cutest sifdecode
   do
-    cp $($brewcmd --prefix $f)/$f.bashrc .
+    cat $(brew --prefix $f)/$f.bashrc >> $TRAVIS_BUILD_DIR/src_vars
   done
-  sudo ln -s $($brewcmd --prefix cutest)/lib/libcutest.so /usr/local/lib/
+  sudo ln -s $(brew --prefix cutest)/lib/libcutest.so /usr/local/lib/
 else
   # Implement before_install for OSX here
   echo "No support for OSX" yet
