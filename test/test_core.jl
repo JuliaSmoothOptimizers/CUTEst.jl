030b3510e5fb3de8e4ed085e7bdaadc409fe63d5
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2015-03-07 17:47:45 -0300

    Test cgr on core interface

--- test/test_core.jl
+++ test/test_core.jl
@@ -10,7 +10,8 @@ glx = Array(Cdouble, nvar[1])
 gval = Array(Cdouble, nvar[1])
 gvar = Array(Cint, nvar[1])
 nnzg = Cint[0]
-grad = Cint[true]
+True = Cint[true]
+False = Cint[false]
 Jx = Array(Cdouble, ncon[1],nvar[1])
 Jtx = Array(Cdouble, nvar[1],ncon[1])
 
@@ -19,11 +20,11 @@ if (ncon[1] > 0)
   @test_approx_eq_eps fx[1] f(x0) 1e-8
   @test_approx_eq_eps cx c(x0) 1e-8
 
-  CUTEst.cofg(st, nvar, x0, fx, gx, grad, nlp.libname)
+  CUTEst.cofg(st, nvar, x0, fx, gx, True, nlp.libname)
   @test_approx_eq_eps fx[1] f(x0) 1e-8
   @test_approx_eq_eps gx g(x0) 1e-8
 
-  CUTEst.cofsg(st, nvar, x0, fx, nnzg, nvar, gval, gvar, grad, nlp.libname)
+  CUTEst.cofsg(st, nvar, x0, fx, nnzg, nvar, gval, gvar, True, nlp.libname)
   @test_approx_eq_eps fx[1] f(x0) 1e-8
   gx = zeros(nvar[1])
   for i = 1:nnzg[1]
@@ -31,14 +32,31 @@ if (ncon[1] > 0)
   end
   @test_approx_eq_eps gx g(x0) 1e-8
 
-  CUTEst.ccfg(st, nvar, ncon, x0, cx, Cint[true], nvar, ncon, Jtx, grad, nlp.libname)
+  CUTEst.ccfg(st, nvar, ncon, x0, cx, True, nvar, ncon, Jtx, True, nlp.libname)
   @test_approx_eq_eps Jtx J(x0)' 1e-8
-  CUTEst.ccfg(st, nvar, ncon, x0, cx, Cint[false], ncon, nvar, Jx, grad, nlp.libname)
+  CUTEst.ccfg(st, nvar, ncon, x0, cx, False, ncon, nvar, Jx, True, nlp.libname)
   @test_approx_eq_eps Jx J(x0) 1e-8
 
-  CUTEst.clfg(st, nvar, ncon, x0, y0, lx, glx, grad, nlp.libname)
-  @test_approx_eq_eps lx f(x0)+dot(y0,cx) 1e-8
+  CUTEst.clfg(st, nvar, ncon, x0, y0, lx, glx, True, nlp.libname)
+  @test_approx_eq_eps lx[1] f(x0)+dot(y0,cx) 1e-8
   @test_approx_eq_eps glx g(x0)+J(x0)'*y0 1e-8
+
+  CUTEst.cgr(st, nvar, ncon, x0, y0, False, gx, True, nvar, ncon, Jtx,
+      nlp.libname)
+  @test_approx_eq_eps gx g(x0) 1e-8
+  @test_approx_eq_eps Jtx J(x0)' 1e-8
+  CUTEst.cgr(st, nvar, ncon, x0, y0, False, gx, False, ncon, nvar, Jx,
+      nlp.libname)
+  @test_approx_eq_eps gx g(x0) 1e-8
+  @test_approx_eq_eps Jx J(x0) 1e-8
+  CUTEst.cgr(st, nvar, ncon, x0, y0, True, glx, True, nvar, ncon, Jtx,
+      nlp.libname)
+  @test_approx_eq_eps glx g(x0)+J(x0)'*y0 1e-8
+  @test_approx_eq_eps Jtx J(x0)' 1e-8
+  CUTEst.cgr(st, nvar, ncon, x0, y0, True, glx, False, ncon, nvar, Jx,
+      nlp.libname)
+  @test_approx_eq_eps glx g(x0)+J(x0)'*y0 1e-8
+  @test_approx_eq_eps Jx J(x0) 1e-8
 else
   CUTEst.ufn(st, nvar, x0, fx, nlp.libname)
   @test_approx_eq_eps fx[1] f(x0) 1e-8
