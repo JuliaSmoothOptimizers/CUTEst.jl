029c3e258495c0a22ea7da71eec759b9cbc57762
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2016-09-19 14:12:28 -0300

    Start development of the docs

---
+++ docs/make.jl
@@ -0,0 +1,11 @@
+using Documenter, CUTEst, NLPModels
+
+makedocs(
+  modules = [CUTEst]
+)
+
+deploydocs(deps = Deps.pip("pygmenters", "mkdocs", "mkdocs-material", "python-markdown-math"),
+  repo = "github.com/JuliaSmoothOptimizers/CUTEst.jl.git",
+  julia = "release",
+  latest = "develop"
+)
