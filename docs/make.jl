30d4576585c9665a675538b5295c202b4d420b9c
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2016-09-20 09:20:23 -0300

    Fix pygments

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs(
   modules = [CUTEst]
 )
 
-deploydocs(deps = Deps.pip("pygmenters", "mkdocs", "mkdocs-material", "python-markdown-math"),
+deploydocs(deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
   repo = "github.com/JuliaSmoothOptimizers/CUTEst.jl.git",
   julia = "release",
   latest = "docs"
