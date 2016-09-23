b4128b657ae96533a55b09696f88eb56ec6267e0
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2016-09-23 09:34:52 -0300

    Test only against JuMP, so that future changes won't affect this much

--- test/consistency.jl
+++ test/consistency.jl
@@ -9,9 +9,8 @@ for problem in problems
 
   problem_f = eval(problem)
   nlp_jump = JuMPNLPModel(problem_f())
-  nlp_simple = eval(parse("$(problem)_simple"))()
   nlp_cutest = CUTEstModel(uppercase(problem_s))
-  nlps = [nlp_cutest, nlp_jump, nlp_simple]
+  nlps = [nlp_cutest, nlp_jump]
 
   @printf("Checking problem %-15s%12s\t", problem_s, "")
   consistent_nlps(nlps)
