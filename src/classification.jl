6071d21168410723818d69492b52c7731f723304
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-04-15 12:10:58 -0300

    Update the classification to include new problems (except BROWDENE and DMN15102LS)
    BROWDENE has a typo (https://gitlab.com/dpo/mastsif-mirror/issues/1),
    and DMN15102LS has a wrong classification origin
    (https://gitlab.com/dpo/mastsif-mirror/issues/2).

--- src/classification.jl
+++ src/classification.jl
@@ -35,43 +35,48 @@ it is not updated. If you do, please open an issue at
 function create_class()
   classdb = open(readlines, joinpath(ENV["MASTSIF"], "CLASSF.DB"))
   problems = Dict()
+  nlp = 0
   for line in classdb
     sline = split(line)
-    p = sline[1]
-    cl = sline[2]
+    p = String(sline[1])
+    cl = split(sline[2], "")
 
     print("Problem $p... ")
-    nlp = CUTEstModel(p)
-    problems[p] = Dict(
-        :objtype          => classdb_objtype[cl[1]],
-        :contype          => classdb_contype[cl[2]],
-        :regular          => cl[3] == 'R',
-        :derivative_order => Int(cl[4]-'0'),
-        :origin           => classdb_origin[cl[6]],
-        :has_internal_var => cl[7] == 'Y',
-        :variables        => Dict(
-          :can_choose     => sline[3][1] == 'V',
-          :number         => nlp.meta.nvar,
-          :fixed          => length(nlp.meta.ifix),
-          :free           => length(nlp.meta.ifree),
-          :bounded_below  => length(nlp.meta.ilow),
-          :bounded_above  => length(nlp.meta.iupp),
-          :bounded_both   => length(nlp.meta.irng)
-        ),
-        :constraints      => Dict(
-          :can_choose     => sline[4][1] == 'V',
-          :number         => nlp.meta.ncon,
-          :equality       => length(nlp.meta.jfix),
-          :ineq_below     => length(nlp.meta.jlow),
-          :ineq_above     => length(nlp.meta.jupp),
-          :ineq_both      => length(nlp.meta.jrng),
-          :linear         => nlp.meta.nlin,
-          :nonlinear      => nlp.meta.nnln
-        )
-    )
-
-    finalize(nlp)
-    println("done")
+    try
+      nlp = CUTEstModel(p)
+      problems[p] = Dict(
+          :objtype          => classdb_objtype[cl[1]],
+          :contype          => classdb_contype[cl[2]],
+          :regular          => cl[3] == "R",
+          :derivative_order => Int(cl[4][1]-'0'),
+          :origin           => classdb_origin[cl[6]],
+          :has_internal_var => cl[7] == "Y",
+          :variables        => Dict(
+            :can_choose     => sline[3][1] == "V",
+            :number         => nlp.meta.nvar,
+            :fixed          => length(nlp.meta.ifix),
+            :free           => length(nlp.meta.ifree),
+            :bounded_below  => length(nlp.meta.ilow),
+            :bounded_above  => length(nlp.meta.iupp),
+            :bounded_both   => length(nlp.meta.irng)
+          ),
+          :constraints      => Dict(
+            :can_choose     => sline[4][1] == "V",
+            :number         => nlp.meta.ncon,
+            :equality       => length(nlp.meta.jfix),
+            :ineq_below     => length(nlp.meta.jlow),
+            :ineq_above     => length(nlp.meta.jupp),
+            :ineq_both      => length(nlp.meta.jrng),
+            :linear         => nlp.meta.nlin,
+            :nonlinear      => nlp.meta.nnln
+          )
+      )
+      println("done")
+    catch ex
+      println("errored: $ex")
+    finally
+      finalize(nlp)
+    end
   end
   open(joinpath(dirname(@__FILE__), "classf.json"), "w") do jsonfile
     JSON.print(jsonfile, problems)
