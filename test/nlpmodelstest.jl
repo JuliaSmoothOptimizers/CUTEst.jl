56c4c6cc05b94d1cd27c282ebec2fc27509980b6
tmigot <tangi.migot@gmail.com>
2022-08-18 19:10:54 +0200

    add `linear_api` tests

--- test/nlpmodelstest.jl
+++ test/nlpmodelstest.jl
@@ -4,9 +4,9 @@ for problem in problems
     nlp_manual = eval(Symbol(problem))()
     nlps = [nlp_cutest, nlp_manual]
 
-    consistent_nlps(nlps)
+    consistent_nlps(nlps, linear_api = true, reimplemented = ["cons","jac", "jprod", "jtprod"])
     view_subarray_nlp(nlp_cutest)
-    coord_memory_nlp(nlp_cutest)
+    coord_memory_nlp(nlp_cutest, linear_api = true)
 
     finalize(nlp_cutest)
   end
