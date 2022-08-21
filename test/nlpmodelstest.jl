c68787e5ad2bb13c404513ed88412c3ecdb4aaf0
tmigot <tmigot@users.noreply.github.com>
2022-08-21 18:18:55 +0000

    :robot: Format .jl files

--- test/nlpmodelstest.jl
+++ test/nlpmodelstest.jl
@@ -4,7 +4,7 @@ for problem in problems
     nlp_manual = eval(Symbol(problem))()
     nlps = [nlp_cutest, nlp_manual]
 
-    consistent_nlps(nlps, linear_api = true, reimplemented = ["cons","jac", "jprod", "jtprod"])
+    consistent_nlps(nlps, linear_api = true, reimplemented = ["cons", "jac", "jprod", "jtprod"])
     view_subarray_nlp(nlp_cutest)
     coord_memory_nlp(nlp_cutest, linear_api = true)
 
