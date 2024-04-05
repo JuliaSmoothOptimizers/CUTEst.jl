9a373ed1d1b32203780c1f8379d4f8ebcc5ce7e1
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-04-05 00:26:48 -0400

    Use CUTEst_jll.jl v2.0.7 with Windows support

--- src/CUTEst.jl
+++ src/CUTEst.jl
@@ -62,7 +62,7 @@ struct CUTEstException <: Exception
   end
 end
 
-const cutest_problems_path = joinpath(dirname(@__FILE__), "../deps", "files")
+const cutest_problems_path = joinpath(dirname(@__FILE__), "..", "deps", "files")
 isdir(cutest_problems_path) || mkpath(cutest_problems_path)
 global cutest_lib = C_NULL
 
@@ -75,10 +75,6 @@ function __init__()
     return
   end
 
-  ENV["ARCHDEFS"] = joinpath(CUTEst_jll.artifact_dir, "ARCHDefs")
-  ENV["SIFDECODE"] = joinpath(CUTEst_jll.artifact_dir, "SIFDecode")
-  ENV["CUTEST"] = joinpath(CUTEst_jll.artifact_dir, "CUTEst")
-
   # set default MASTSIF location if the user hasn't set it already
   if !("MASTSIF" ∈ keys(ENV))
     ENV["MASTSIF"] = joinpath(artifact"sifcollection", "optrove-sif-99c5b38e7d03")
@@ -87,27 +83,6 @@ function __init__()
   end
   @info "using problem repository" ENV["MASTSIF"]
 
-  # Set MYARCH
-  if Sys.isapple()
-    if Sys.WORD_SIZE == 64
-      ENV["MYARCH"] = "mac64.osx.gfo"
-    else
-      ENV["MYARCH"] = "mac.osx.gfo"
-    end
-  elseif Sys.iswindows()
-    if Sys.WORD_SIZE == 64
-      ENV["MYARCH"] = "pc64.mgw.gfo"
-    else
-      ENV["MYARCH"] = "pc.mgw.gfo"
-    end
-  else
-    ENV["MYARCH"] = "pc64.lnx.gfo"
-    # if Sys.WORD_SIZE == 64
-    #   ENV["MYARCH"] = "pc64.lnx.gfo"
-    # else
-    #   ENV["MYARCH"] = "pc.lnx.gfo"
-    # end
-  end
   global libpath = joinpath(CUTEst_jll.artifact_dir, "lib")
   push!(Libdl.DL_LOAD_PATH, cutest_problems_path)
 end
