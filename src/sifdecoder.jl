1f538cf22be3e4727f224da7f515dc60d9a44756
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-08-17 13:13:54 -0400

    Update the sifcollection in Artifacts.toml

--- src/sifdecoder.jl
+++ src/sifdecoder.jl
@@ -10,7 +10,7 @@ The supported sets are:
 """
 function set_mastsif(set::String = "sifcollection")
   if set == "sifcollection"
-    ENV["MASTSIF"] = joinpath(artifact"sifcollection", "optrove-sif-229e00b81891")
+    ENV["MASTSIF"] = joinpath(artifact"sifcollection", "optrove-sif-c71425cc7f54")
   elseif set == "maros-meszaros"
     ENV["MASTSIF"] = joinpath(artifact"maros-meszaros", "optrove-maros-meszaros-9adfb5707b1e")
   elseif set == "netlib-lp"
