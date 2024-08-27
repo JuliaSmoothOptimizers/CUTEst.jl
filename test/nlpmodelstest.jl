e1a1fcce68178c5ad88b5005cc91e9dd8150c364
amontoison <amontoison@users.noreply.github.com>
2024-08-27 22:47:12 +0000

    :robot: Format .jl files

--- test/nlpmodelstest.jl
+++ test/nlpmodelstest.jl
@@ -4,7 +4,12 @@ for problem in problems
     nlp_manual = eval(Symbol(problem))()
     nlps = [nlp_cutest, nlp_manual]
 
-    consistent_nlps(nlps, linear_api = true, reimplemented = ["cons", "jac", "jprod", "jtprod"], exclude = [jth_hprod, ghjvprod])
+    consistent_nlps(
+      nlps,
+      linear_api = true,
+      reimplemented = ["cons", "jac", "jprod", "jtprod"],
+      exclude = [jth_hprod, ghjvprod],
+    )
     view_subarray_nlp(nlp_cutest)
     coord_memory_nlp(nlp_cutest, linear_api = true)
 
