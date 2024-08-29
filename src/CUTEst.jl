4cf1a12741b566edf13c9c6b3ce4c658ee4107da
amontoison <amontoison@users.noreply.github.com>
2024-08-29 22:07:44 +0000

    :robot: Format .jl files

--- src/CUTEst.jl
+++ src/CUTEst.jl
@@ -13,7 +13,8 @@ import REPL.TerminalMenus
 import Base.format_bytes
 import Printf.@sprintf
 
-export CUTEstModel, sifdecoder, build_libsif, set_mastsif, clear_libsif, manage_libsif, list_sif_problems
+export CUTEstModel,
+  sifdecoder, build_libsif, set_mastsif, clear_libsif, manage_libsif, list_sif_problems
 
 const cutest_false = Ref{Bool}(false)
 const cutest_true = Ref{Bool}(true)
