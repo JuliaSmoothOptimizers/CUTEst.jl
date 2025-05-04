bbc50a00e6da931284d1e362bad98857a82d7c97
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-05-03 23:28:38 -0500

    Update allocation tests

--- test/test_allocations.jl
+++ test/test_allocations.jl
@@ -1,10 +1,12 @@
+# using CUTEst, NLPModelsTest, NLPModels, Quadmath
+
 @testset "allocations -- $pb" for pb in ("BROWNDEN", "HS5", "HS6", "HS10", "HS11", "HS13", "HS14")
     @testset "precision -- $T" for T in (Float32, Float64, Float128)
         (T == Float128) && (Sys.ARCH == :aarch64) && Sys.islinux() && continue
         nlp = CUTEstModel{T}(pb)
-        NLPModelsTest.test_zero_allocations(nlp; linear_api=true)
-        mems = NLPModelsTest.test_allocs_nlpmodels(nlp; linear_api=true)
-        display(mems)
+        NLPModelsTest.test_zero_allocations(nlp; linear_api=true, exclude=[jac_op])
+        # mems = NLPModelsTest.test_allocs_nlpmodels(nlp; linear_api=true, exclude=[jac_op])
+        # display(mems)
         finalize(nlp)
     end
 end
